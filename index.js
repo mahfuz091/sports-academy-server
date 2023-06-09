9ab812cbd0819d3150ed89091e9bd98a5dbc4be1
mahfuz091 <engmahfuz14@gmail.com>
2023-06-09 21:30:50 +0600

    verifyJwt function added

--- index.js
+++ index.js
@@ -9,6 +9,23 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
+  }
+
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1maxaaz.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -29,6 +46,14 @@ async function run() {
     const classesCollection = client.db("sportscampDB").collection("classes");
     const cartCollection = client.db("sportscampDB").collection("carts");
     const usersCollection = client.db("sportscampDB").collection("users");
+
+    // jwt apis
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+
+      res.send({ token })
+    })
 
     app.get("/all-classes", async (req, res) => {
       const result = await classesCollection.find().toArray();
