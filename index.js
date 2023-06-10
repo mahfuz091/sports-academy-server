59787450e5576a5494bc04c5af46ac011c5c5c27
mahfuz091 <engmahfuz14@gmail.com>
2023-06-10 20:34:29 +0600

    pending classes apis added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
 const app = express();
+const cors = require("cors");
+
+require("dotenv").config();
+
 const jwt = require("jsonwebtoken");
 
 const port = process.env.PORT || 5000;
@@ -45,7 +47,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const classesCollection = client.db("sportscampDB").collection("classes");
     const cartCollection = client.db("sportscampDB").collection("carts");
@@ -90,6 +92,12 @@ async function run() {
     };
 
     app.get("/all-classes", async (req, res) => {
+      const query = { status: "approved" };
+      const result = await classesCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.get("/pending-classes", async (req, res) => {
+
       const result = await classesCollection.find().toArray();
       res.send(result);
     });
@@ -99,6 +107,37 @@ async function run() {
       const result = await classesCollection.insertOne(newItem)
       res.send(result);
     })
+
+    app.patch("/all-classes/approved/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          status: "approved"
+
+        },
+      };
+
+
+      const result = await classesCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+    app.patch("/all-classes/deny/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          status: "denied"
+
+        },
+      };
+
+
+      const result = await classesCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
 
     // users related apis
     app.get(
@@ -122,20 +161,20 @@ async function run() {
       }
 
       const result = await usersCollection.insertOne(user);
-      res.send(result);
+      return res.send(result);
     });
 
     app.get("/users/admin/:email", verifyJWT, async (req, res) => {
       const email = req.params.email;
 
       if (req.decoded.email !== email) {
-        res.send({ admin: false });
+        return res.send({ admin: false });
       }
 
       const query = { email: email };
       const user = await usersCollection.findOne(query);
       const result = { admin: user?.role === "admin" };
-      res.send(result);
+      return res.send(result);
     });
 
     app.patch("/users/admin/:id", async (req, res) => {
@@ -156,13 +195,13 @@ async function run() {
       const email = req.params.email;
 
       if (req.decoded.email !== email) {
-        res.send({ instructor: false });
+        return res.send({ instructor: false });
       }
 
       const query = { email: email };
       const user = await usersCollection.findOne(query);
       const result = { instructor: user?.role === "instructor" };
-      res.send(result);
+      return res.send(result);
     });
 
     app.patch("/users/instructor/:id", async (req, res) => {
@@ -198,11 +237,11 @@ async function run() {
       const email = req.query.email;
 
       if (!email) {
-        res.send([]);
+        return res.send([]);
       }
       const query = { email: email };
       const result = await cartCollection.find(query).toArray();
-      res.send(result);
+      return res.send(result);
     });
 
     app.delete("/carts/:id", async (req, res) => {
@@ -223,7 +262,7 @@ async function run() {
       }
 
       const result = await cartCollection.insertOne(item);
-      res.send(result);
+      return res.send(result);
     });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
