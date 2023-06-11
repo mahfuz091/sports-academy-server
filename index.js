e02f9d48cd8f5f1fbae6427ae7f4922bda51c1af
mahfuz091 <engmahfuz14@gmail.com>
2023-06-11 11:04:31 +0600

    payment get api created

--- index.js
+++ index.js
@@ -332,6 +332,10 @@ async function run() {
       const result = await classesCollection.updateOne(filter, update);
       res.send(result);
     });
+    app.get('/payments', async (req, res) => {
+      const result = await paymentCollection.find().toArray()
+      res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
